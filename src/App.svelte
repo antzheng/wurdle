f6fe2fd38be7b46bb5fa22daafb828f1748aabe6
Anthony Zheng <anthonyzheng0290@gmail.com>
2022-02-01 14:46:05 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Header from './components/Header.svelte';
+  import GameBoard from './components/GameBoard.svelte';
+  import Keyboard from './components/Keyboard.svelte';
+</script>
+
+<div id="game">
+  <Header />
+  <GameBoard />
+  <Keyboard />
+</div>
+
+<style lang="scss">
+  #game {
+    width: 100%;
+    max-width: var(--app-max-width);
+    margin: 0 auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,67 @@
+<script>
+  import HelpIcon from 'svelte-icons/io/IoMdHelpCircle.svelte';
+  import StatsIcon from 'svelte-icons/io/IoMdStats.svelte';
+  import SettingsIcon from 'svelte-icons/io/IoMdSettings.svelte';
+</script>
+
+<header class="navbar">
+  <div class="navbar-content">
+    <div class="menu navbar-left">
+      <button class="button"><HelpIcon /></button>
+    </div>
+    <div class="title">WURDLE</div>
+    <div class="menu navbar-right">
+      <button class="button"><StatsIcon /></button>
+      <button class="button"><SettingsIcon /></button>
+    </div>
+  </div>
+</header>
+
+<style lang="scss">
+  .navbar {
+    width: 100%;
+    height: var(--header-height);
+    color: var(--color-tone-1);
+    border-bottom: 1px solid var(--color-tone-4);
+    display: flex;
+    align-items: center;
+  }
+
+  .navbar-content {
+    width: 100%;
+    max-width: var(--game-max-width);
+    margin: 0 auto;
+    padding: 0px 25px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .navbar-left {
+    justify-content: flex-start;
+  }
+
+  .navbar-right {
+    justify-content: flex-end;
+  }
+
+  .menu {
+    display: flex;
+    align-items: center;
+    min-width: 100px;
+  }
+
+  .title {
+    font-weight: 700;
+    font-size: 36px;
+    letter-spacing: 0.2rem;
+    text-transform: uppercase;
+    pointer-events: none;
+  }
+
+  .button {
+    width: 30px;
+    height: 30px;
+    padding: 3px;
+  }
+</style>
